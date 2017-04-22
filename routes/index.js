91b7b508406f258091014f2de9131a8dc38d44ef
Yomi <master.yummy123@gmail.com>
2017-04-22 23:59:30 +0100

    changes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 module.exports = function(app){
   var accepts = require('accepts');
-  app.get('/', (req, res) => {
+  var API = '/whoami';
+  app.get( API , (req, res) => {
     var ip = req.ip;
     var language = accepts(req).languages()[0];
-    var software = "OS: " + " " + req.useragent.os +" " + "platform: "+ " " + req.useragent.source;
+    var software = "OS: " + " " + req.useragent.os + " " + "platform: "+ " " + req.useragent.source;
 
     res.json({ "ipaddress" : ip, "language": language, "software": software })
   })
